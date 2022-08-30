b938fcabe276b756a17a6bbd2134a6001e1f7446
Alexander David <alexander-david-dev@yandex.com>
2022-08-30 22:26:15 +0300

    removed unnecessary version check

--- src/__main__.py
+++ src/__main__.py
@@ -3,11 +3,5 @@
 import sys
 
 if __name__ == "__main__":
-    pyv = sys.version.split()[0]
-
-    if sys.version_info < (3, 7):
-        print("pie requires python 3.7, you are using %s" % (pyv))
-        sys.exit(1)
-
     import pie
     pie.main()
